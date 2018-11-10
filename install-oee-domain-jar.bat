32c81c6e6d20770835e8782b6f16260554c1b860
billran <kentra@pacbell.net>
2018-11-09 16:35:57 -0800

    Updated the Milo, Hibernate and RabbitMQ dependencies

--- install-oee-domain-jar.bat
+++ install-oee-domain-jar.bat
@@ -2,4 +2,4 @@ rem Maven install script for OEE Domain jar
 call mvn clean
 call mvn package
 rem install jar in local repo
-call mvn install:install-file -Dfile=C:/dev/OEE-Domain/target/OEE-Domain-1.1.0.jar -DgroupId=org.point85 -DartifactId=oee-domain -Dversion=1.1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=C:/dev/OEE-Domain/target/OEE-Domain-1.1.1.jar -DgroupId=org.point85 -DartifactId=oee-domain -Dversion=1.1.1 -Dpackaging=jar
